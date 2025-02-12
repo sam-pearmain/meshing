e922d1c8c778488a1f2bd5af2b1f60b942c09dc1
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-02-12 16:19:45 +0000

    just added a random float test

--- src/geometry/tests.rs
+++ src/geometry/tests.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
     use crate::geometry::vertex::Vertex;
-    
+
     #[test]
     fn test_vertex_creation() {
         let vertex = Vertex {
@@ -13,5 +13,15 @@ mod tests {
         assert_eq!(vertex.id, 1);
         assert_eq!(vertex.x, 0.0);
         assert_eq!(vertex.y, 0.0);
+    }
+
+    #[test]
+    fn add_f32_f64() {
+        let float1: f32 = 1.0;
+        let float2: f64 = 2.0;
+
+        // floats have to be of the same type to implement add
+        let result = float1 as f64 + float2;
+        assert_eq!(result, 3.0);
     }
 }
